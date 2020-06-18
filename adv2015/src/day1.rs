e461a246ab486c5b62aacec5516428ac736fdef7
Vest <Vest@users.noreply.github.com>
2020-06-18 17:24:16 +0200

    Advent 2015! Day1 / 2015: 138 and 1771 Done
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- adv2015/src/day1.rs
+++ adv2015/src/day1.rs
@@ -1,10 +1,16 @@
 use reqwest::header::{HeaderMap, HeaderValue};
 
-pub fn get_answer(session: String) -> i16{
+pub fn get_answer1(session: String) -> i16 {
     let body = get_input(session);
     let count = count_brackets(body);
 
     count
+}
+
+pub fn get_answer2(session: String) -> i16 {
+    let body = get_input(session);
+    let position = count_position(body);
+    position
 }
 
 fn count_brackets(s: String) -> i16 {
@@ -20,6 +26,30 @@ fn count_brackets(s: String) -> i16 {
     });
 
     floor
+}
+
+fn count_position(s: String) -> i16 {
+    let mut sum: i16 = 0;
+    let mut counter: i16 = 1;
+    let mut chars = s.chars();
+
+    while let Some(c) = chars.next() {
+        sum += if c == '(' {
+            1
+        } else if c == ')' {
+            -1
+        } else {
+            0
+        };
+
+        if sum < 0 {
+            return counter;
+        } else {
+            counter += 1;
+        }
+    }
+
+    return 0;
 }
 
 fn get_input(session: String) -> String {
@@ -52,21 +82,21 @@ fn get_input(session: String) -> String {
 
 #[cfg(test)]
 mod tests {
-    use crate::day1::count_brackets;
-    use crate::day1::get_input;
+    use crate::day1::{count_brackets, count_position};
 
     #[test]
     fn test_count_brackets() {
-        assert_eq!(count_brackets("((("), 3);
-        assert_eq!(count_brackets(")))"), -3);
-        assert_eq!(count_brackets("()(())"), 0);
-        assert_eq!(count_brackets("((())"), 1);
-        assert_eq!(count_brackets("(((a)b)c"), 1);
+        assert_eq!(count_brackets(String::from("(((")), 3);
+        assert_eq!(count_brackets(String::from(")))")), -3);
+        assert_eq!(count_brackets(String::from("()(())")), 0);
+        assert_eq!(count_brackets(String::from("((())")), 1);
+        assert_eq!(count_brackets(String::from("(((a)b)c")), 1);
     }
 
     #[test]
-    fn test_get() {
-        assert_ne!(get_input().len(), 0, "Couldn't send GET as a HTTP request");
+    fn test_count_position() {
+        assert_eq!(count_position(String::from(")")), 1);
+        assert_eq!(count_position(String::from("()())")), 5);
     }
 }
 
--- adv2015/src/lib.rs
+++ adv2015/src/lib.rs
@@ -1,20 +1,17 @@
-use crate::day1::get_answer;
+use crate::day1::{get_answer1, get_answer2};
 
 pub mod day1;
 
 pub fn print_answers(session: String) {
-    println!("Day1 / 2015: {}", get_answer(session));
+    println!("Day1 / 2015: {} and {}", get_answer1(session.clone()), get_answer2(session.clone()));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day1::print_answer;
-
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
-
 }
 
 
