0c984ad22184c2a25e2f99abd29c8c32273e35bb
Vest <Vest@users.noreply.github.com>
2020-06-19 14:35:33 +0200

    Advent 2015! Day 1 / 2015: 138 and 1771 Day 2 / 2015: 1586300 and 3737498
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- adv2015/src/day2.rs
+++ adv2015/src/day2.rs
@@ -1,3 +1,37 @@
+pub fn calc_packs(s: &str) -> u32 {
+    s.lines()
+        .map(|line| {
+            let (l, w, h) = parse_line(line);
+            return calc_paper(l, w, h);
+        })
+        .sum()
+}
+
+pub fn calc_ribbons(s: &str) -> u32 {
+    s.lines()
+        .map(|line| {
+            let (l, w, h) = parse_line(line);
+            return calc_ribbon(l, w, h);
+        })
+        .sum()
+}
+
+fn parse_line(s: &str) -> (u8, u8, u8) {
+    let mut result: Vec<u8> = Vec::new();
+    let tokens = s.split('x');
+    for token in tokens {
+        if let Ok(dim) = token.parse::<u8>() {
+            result.push(dim);
+        }
+    }
+
+    if result.len() != 3 {
+        return (0, 0, 0);
+    }
+
+    (result[0], result[1], result[2])
+}
+
 fn calc_paper(l: u8, w: u8, h: u8) -> u32 {
     let dim = (l as u32, w as u32, h as u32);
     let mut sorted_dim = [l as u32, w as u32, h as u32];
@@ -6,9 +40,17 @@ fn calc_paper(l: u8, w: u8, h: u8) -> u32 {
     2 * (dim.0 * dim.1 + dim.1 * dim.2 + dim.0 * dim.2) + sorted_dim[0] * sorted_dim[1]
 }
 
+fn calc_ribbon(l: u8, w: u8, h: u8) -> u32 {
+    let dim = (l as u32, w as u32, h as u32);
+    let mut sorted_dim = [l as u32, w as u32, h as u32];
+    sorted_dim.sort();
+
+    dim.0 * dim.1 * dim.2 + 2 * (sorted_dim[0] + sorted_dim[1])
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day2::{calc_paper};
+    use crate::day2::{calc_paper, parse_line, calc_packs, calc_ribbon, calc_ribbons};
 
     #[test]
     fn test_calc_paper_random() {
@@ -26,5 +68,29 @@ mod tests {
     #[test]
     fn test_calc_paper_max() {
         assert_eq!(calc_paper(255, 255, 255), 455175);
+    }
+
+    #[test]
+    fn test_parse_line() {
+        assert_eq!(parse_line("1x1x1"), (1, 1, 1));
+        assert_eq!(parse_line("1x2x3"), (1, 2, 3));
+        assert_eq!(parse_line("1x2x3x4"), (0, 0, 0));
+        assert_eq!(parse_line("1x2"), (0, 0, 0));
+    }
+
+    #[test]
+    fn test_calc_packs() {
+        assert_eq!(calc_packs("2x3x4\n1x1x10"), 58 + 43);
+    }
+
+    #[test]
+    fn test_calc_ribbon() {
+        assert_eq!(calc_ribbon(2, 3, 4), 34);
+        assert_eq!(calc_ribbon(1, 1, 10), 14);
+    }
+
+    #[test]
+    fn test_calc_ribbons() {
+        assert_eq!(calc_ribbons("2x3x4\n1x1x10"), 34 + 14);
     }
 }
--- adv2015/src/lib.rs
+++ adv2015/src/lib.rs
@@ -6,7 +6,7 @@ pub fn print_answers(day: u8, input: String) {
     let prefix = format!("Day {} / 2015:", day);
     match day {
         1 => println!("{} {} and {}", prefix, day1::count_brackets(input), day1::count_position(input)),
-        2 => println!("{} {}", prefix, ""),
+        2 => println!("{} {} and {}", prefix, day2::calc_packs(input), day2::calc_ribbons(input)),
         _ => { eprintln!("2015: I don't know the answer for day {} :(", day) }
     }
 }
