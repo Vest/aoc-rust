f1738edb65b040062552067f484260cfce831340
Vest <Vest@users.noreply.github.com>
2020-06-30 18:56:33 +0200

    Day 8 / 2015: 1342 and 1342
    very unsafe code, no error checks.
    
    Signed-off-by: Vest <Vest@users.noreply.github.com>

---
+++ adv2015/src/day8.rs
@@ -0,0 +1,119 @@
+use std::char;
+
+pub fn calc_difference(input: &str) -> usize {
+    let literals = input.lines()
+        .fold(0, |acc, line| {
+            acc + count_literals(line)
+        });
+
+    let chars = input.lines()
+        .fold(0, |acc, line| {
+            acc + count_chars(line)
+        });
+
+    literals - chars
+}
+
+fn count_literals(input: &str) -> usize {
+    input.chars()
+        .count()
+}
+
+fn count_chars(input: &str) -> usize {
+    unescape(input).chars()
+        .count()
+}
+
+fn unescape(input: &str) -> String {
+    let mut in_single_quote = false;
+    let mut in_double_quote = false;
+
+    let mut chars = input.chars().enumerate();
+
+    let mut res = String::with_capacity(input.len());
+
+    while let Some((idx, c)) = chars.next() {
+        if in_single_quote {
+            if c == '\'' {
+                in_single_quote = false;
+                continue;
+            }
+        } else if in_double_quote {
+            if c == '"' {
+                in_double_quote = false;
+                continue;
+            }
+
+            if c == '\\' {
+                match chars.next() {
+                    None => return String::new(),
+                    Some((idx, c2)) => {
+                        res.push(match c2 {
+                            '\\' => '\\',
+                            '\'' => '\'',
+                            '"' => '"',
+                            'x' => parse_unicode(&mut chars),
+                            _ => {
+                                return String::new();
+                            }
+                        });
+                        continue;
+                    }
+                }
+            }
+        } else if c == '\'' {
+            in_single_quote = true;
+            continue;
+        } else if c == '"' {
+            in_double_quote = true;
+            continue;
+        }
+
+        res.push(c);
+    }
+
+    res
+}
+
+fn parse_unicode<I>(chars: &mut I) -> char
+    where
+        I: Iterator<Item=(usize, char)> {
+    let c1 = chars.next().unwrap().1;
+    let c2 = chars.next().unwrap().1;
+
+    let d1 = c1.to_digit(16).unwrap();
+    let d2 = c2.to_digit(16).unwrap();
+
+    let hex = d1 * 16 + d2;
+
+    char::from_u32(hex).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_count_literals() {
+        assert_eq!(count_literals("\"\""), 2);
+        assert_eq!(count_literals("\"abc\""), 5);
+        assert_eq!(count_literals("\"aaa\\\"aaa\""), 10);
+        assert_eq!(count_literals("\"\\x27\""), 6);
+    }
+
+    #[test]
+    fn test_unescape() {
+        assert_eq!(unescape("\"\""), "");
+        assert_eq!(unescape("\"abc\""), "abc");
+        assert_eq!(unescape("\"aaa\\\"aaa\""), "aaa\"aaa");
+        assert_eq!(unescape("\"\\x27\""), "'");
+    }
+
+    #[test]
+    fn test_count_chars() {
+        assert_eq!(count_chars("\"\""), 0);
+        assert_eq!(count_chars("\"abc\""), 3);
+        assert_eq!(count_chars("\"aaa\\\"aaa\""), 7);
+        assert_eq!(count_chars("\"\\x27\""), 1);
+    }
+}
