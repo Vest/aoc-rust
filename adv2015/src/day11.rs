c52c7afb945c5d44841fcc42d037505dfc4effa2
Vest <Vest@users.noreply.github.com>
2020-07-07 14:12:42 +0200

    Day 11 / 2015: cqjxxyzz and cqkaabcc
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- adv2015/src/day11.rs
+++ adv2015/src/day11.rs
@@ -1,4 +1,15 @@
+use std::collections::HashSet;
+
 const MAX_LENGTH: usize = 8;
+
+pub fn get_expired_once(input: &str) -> String {
+    find_next(input)
+}
+
+pub fn get_expired_twice(input: &str) -> String {
+    let expired = find_next(input);
+    find_next(expired.as_str())
+}
 
 struct Password {
     state: String,
@@ -23,8 +34,8 @@ impl Password {
         pwd
     }
 
-    fn get_password(&self) -> &str {
-        self.state.as_str()
+    fn get_password(&self) -> String {
+        self.state.clone()
     }
 
     fn inc(&mut self) {
@@ -58,6 +69,84 @@ impl Iterator for Password {
         self.inc();
         Some(self.state.clone())
     }
+}
+
+fn is_increased(input: &str) -> bool {
+    let mut count = 0u8;
+    let mut prev_char: Option<char> = None;
+
+    for c in input.chars() {
+        if prev_char.is_none() {
+            prev_char = Some(c);
+            count = 1;
+            continue;
+        }
+
+        let prev_ascii = prev_char.unwrap() as u8;
+        let c_ascii = c as u8;
+
+        count = if c_ascii.checked_sub(prev_ascii) == Some(1) { count + 1 } else { 1 };
+
+        if count == 3 {
+            return true;
+        }
+
+        prev_char = Some(c);
+    }
+
+    false
+}
+
+fn is_not_confusing(input: &str) -> bool {
+    !input.contains(|c| {
+        c == 'i' || c == 'o' || c == 'l'
+    })
+}
+
+fn has_pairs(input: &str) -> bool {
+    let mut set: HashSet<String> = HashSet::new();
+    let mut prev_char: Option<char> = None;
+    let mut overlap_char: Option<char> = None;
+
+    for c in input.chars() {
+        if prev_char == None {
+            prev_char = Some(c);
+            continue;
+        }
+
+        if prev_char.unwrap() == c {
+            // A potential pair
+            if let Some(overlap) = overlap_char {
+                if overlap == c {
+                    continue;
+                }
+            }
+
+            let mut pair = String::new();
+            pair.push(c);
+            pair.push(c);
+            set.insert(pair);
+        }
+
+        overlap_char = prev_char;
+        prev_char = Some(c);
+    }
+
+    set.len() > 1
+}
+
+fn find_next(input: &str) -> String {
+    let mut pass = Password::new_str(input);
+
+    while let Some(guessed_pass) = pass.next() {
+        let pass_str = guessed_pass.as_str();
+
+        if is_increased(pass_str) && is_not_confusing(pass_str) && has_pairs(pass_str) {
+            break;
+        }
+    }
+
+    pass.get_password()
 }
 
 #[cfg(test)]
@@ -117,5 +206,53 @@ mod tests {
             assert_eq!(pass, "aaaaaaab", "Expected a string with seven 'a' chars and one 'b'");
             break;
         }
+    }
+
+    #[test]
+    fn test_is_increased() {
+        assert!(!is_increased("aaaaaaaa"));
+        assert!(is_increased("abcaaaaa"));
+        assert!(!is_increased("abbceffg"));
+        assert!(!is_increased("abbcegjk"));
+        assert!(is_increased("abcdffaa"));
+        assert!(is_increased("ghjaabcc"));
+        assert!(is_increased("hijklmmn"));
+    }
+
+    #[test]
+    fn test_is_not_confusing() {
+        assert!(!is_not_confusing("hijklmmn"));
+        assert!(is_not_confusing("abbceffg"));
+        assert!(is_not_confusing("abbcegjk"));
+        assert!(is_not_confusing("abcdffaa"));
+        assert!(is_not_confusing("ghjaabcc"));
+    }
+
+    #[test]
+    fn test_has_pairs() {
+        assert!(!has_pairs("hijklmmn"));
+        assert!(has_pairs("abbceffg"));
+        assert!(!has_pairs("abbcegjk"));
+        assert!(has_pairs("abcdffaa"));
+        assert!(has_pairs("ghjaabcc"));
+        assert!(has_pairs("ghjaaabcc"));
+        assert!(has_pairs("ghjaaaabcc"));
+    }
+
+    #[test]
+    fn test_find_next_fast() {
+        let expired_pass = "abcdefgh";
+        let new_pass = find_next(expired_pass);
+
+        assert_eq!(new_pass, "abcdffaa", "I expected another password");
+    }
+
+    #[test]
+    #[ignore]
+    fn test_find_next_slow() {
+        let expired_pass = "ghijklmn";
+        let new_pass = find_next(expired_pass);
+
+        assert_eq!(new_pass, "ghjaabcc", "I expected another password");
     }
 }
--- adv2015/src/lib.rs
+++ adv2015/src/lib.rs
@@ -13,7 +13,8 @@ mod day11;
 /* </editor-fold> */
 
 pub fn print_answers(day: u8, input: String) {
-    let input = input.as_str();
+    // Trim is mandatory for one-line inputs
+    let input = input.as_str().trim();
     let prefix = format!("Day {} / 2015:", day);
     match day {
         1 => println!("{} {} and {}", prefix, day1::count_brackets(input), day1::count_position(input)),
@@ -26,6 +27,7 @@ pub fn print_answers(day: u8, input: String) {
         8 => println!("{} {} and {}", prefix, day8::calc_difference(input), day8::calc_new_difference(input)),
         9 => println!("{} {} and {}", prefix, day9::calc_shortest(input), day9::calc_longest(input)),
         10 => println!("{} {} and {}", prefix, day10::calc_first(input), day10::calc_second(input)),
+        11 => println!("{} {} and {}", prefix, day11::get_expired_once(input), day11::get_expired_twice(input)),
         _ => { eprintln!("2015: I don't know the answer for day {} :(", day) }
     }
 }
