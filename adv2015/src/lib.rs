d7c86106176188d2a9861dd763ee5f70467e2d55
Vest <Vest@users.noreply.github.com>
2020-08-19 12:29:21 +0200

    Day 18 / 2015: 821
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- adv2015/src/lib.rs
+++ adv2015/src/lib.rs
@@ -16,6 +16,7 @@ mod day14;
 mod day15;
 mod day16;
 mod day17;
+mod day18;
 /* </editor-fold> */
 
 pub fn print_answers(day: u8, input: String) {
@@ -40,10 +41,10 @@ pub fn print_answers(day: u8, input: String) {
         15 => println!("{} {} and {}", prefix, day15::get_answer(input), day15::get_answer_with_calories(input)),
         16 => println!("{} {} and {}", prefix, day16::get_answer(input), day16::get_answer_from_retroencabulator(input)),
         17 => println!("{} {} and {}", prefix, day17::get_total_count_of_combinations(input), day17::get_minimal_count_of_cans(input)),
+        18 => println!("{} {} and {}", prefix, day18::get_answer(input), day18::get_answer(input)),
         _ => { eprintln!("2015: I don't know the answer for day {} :(", day) }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
