63e6a18b8191bf83b9807ee5ca597579e8e396c5
Vest <Vest@users.noreply.github.com>
2020-08-29 17:39:17 +0200

    Day 19 / 2015: update version Day 19 / 2015: the second part doesn't work with brute force, but works for smaller input

--- adv2015/src/day19.rs
+++ adv2015/src/day19.rs
@@ -1,11 +1,18 @@
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 pub fn count_unique_molecules(input: &str) -> usize {
     let (replacements, sample_molecule) = parse_all(input);
-    let molecules = build_molecules(sample_molecule, replacements);
+    let molecules = build_molecules(sample_molecule, &replacements);
 
     molecules.len()
+}
+
+pub fn count_molecules_from_electron(input: &str) -> usize {
+    let (replacements, sample_molecule) = parse_all(input);
+    let count = build_molecule_from_e(sample_molecule, &replacements);
+
+    count
 }
 
 struct Replace<'a> {
@@ -47,14 +54,14 @@ fn parse_all(input: &str) -> (Vec<Replace>, &str) {
 }
 
 fn split_molecule(input: &str) -> Vec<&str> {
-    let mol_reg = Regex::new("([A-Z][a-z]?)").unwrap();
+    let mol_reg = Regex::new("(e)|([A-Z][a-d,f-z]?)").unwrap(); // everything but not "Xe". Xa, Xb
 
     mol_reg.captures_iter(input)
         .map(|c| c.get(0).unwrap().as_str())
         .collect()
 }
 
-fn build_molecules(molecule: &str, replacements: Vec<Replace>) -> HashSet<String> {
+fn build_molecules(molecule: &str, replacements: &Vec<Replace>) -> HashSet<String> {
     let mut molecules = HashSet::with_capacity(replacements.capacity());
     let atoms = split_molecule(molecule);
 
@@ -84,18 +91,125 @@ fn build_molecules(molecule: &str, replacements: Vec<Replace>) -> HashSet<String
     molecules
 }
 
+fn build_molecule_from_e(molecule: &str, replacements: &Vec<Replace>) -> usize {
+    let mut mo = build_molecules("e", replacements);
+    let mut iter = 1usize;
+    let replacements_map = convert_replacements(replacements);
+    let molecule_length = molecule.len();
+
+    let mut nonreplaceable_atoms: HashSet<&str> = HashSet::new();
+    split_molecule(molecule).iter()
+        .filter(|atom| !replacements_map.contains_key(*atom))
+        .for_each(|atom| {
+            nonreplaceable_atoms.insert(*atom);
+        });
+
+    let potential_to_optimize = !nonreplaceable_atoms.is_empty();
+    let mut start_optimization: bool = false;
+
+    while !mo.contains(molecule) {
+        let mut new_mo: HashSet<String> = HashSet::new();
+
+        mo.iter()
+            .for_each(|m| new_mo.extend(build_molecules(m, replacements)));
+
+        if potential_to_optimize {
+            if start_optimization {
+                mo.clear();
+                for new_item in new_mo.iter()
+                    .filter(|m| {
+                        let small_atoms = split_molecule(m);
+                        small_atoms.iter()
+                            .find(|p| nonreplaceable_atoms.contains(*p))
+                            .is_some()
+                    }).filter(|m| m.len() <= molecule_length) {
+                    mo.insert(new_item.clone());
+                }
+                println!("Optimized from {} to {}", new_mo.len(), mo.len());
+            } else {
+                new_mo.iter()
+                    .for_each(|m| {
+                        if !start_optimization {
+                            let small_atoms = split_molecule(m);
+                            for small_atom in small_atoms {
+                                if nonreplaceable_atoms.contains(small_atom) {
+                                    start_optimization = true;
+                                    println!("Start optimization");
+                                    break;
+                                }
+                            }
+                        }
+                    });
+            }
+        } else {
+            mo.clear();
+            mo.extend(new_mo);
+        }
+
+
+        iter += 1;
+    }
+
+    iter
+}
+
+fn convert_replacements<'a>(replacements: &'a Vec<Replace>) -> HashMap<&'a str, Vec<&'a str>> {
+    let mut map: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    replacements.iter()
+        .for_each(|replace| {
+            if let Some(to) = map.get_mut(replace.from) {
+                to.push(replace.to);
+            } else {
+                let to = vec![replace.to];
+                map.insert(replace.from, to);
+            }
+        });
+
+    map
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const SAMPLE: &str = r#"H => HO
+            H => OH
+            O => HH
+
+            HOH"#;
+    const SANTA_SAMPLE: &str = r#"H => HO
+            H => OH
+            O => HH
+
+            HOHOHO"#;
+
+    const E_SAMPLE: &str = r#"e => H
+            e => O
+            H => HO
+            H => OH
+            O => HH
+
+            HOH"#;
+
+    const E_SANTA_SAMPLE: &str = r#"e => H
+            e => O
+            H => HO
+            H => OH
+            O => HH
+
+            HOHOHO"#;
+
     #[test]
     fn test_split_molecule() {
-        let atoms = split_molecule("CRnSiRnO");
+        let atoms = split_molecule("CRnSiRneOe");
         assert_eq!(atoms[0], "C");
         assert_eq!(atoms[1], "Rn");
         assert_eq!(atoms[2], "Si");
         assert_eq!(atoms[3], "Rn");
-        assert_eq!(atoms[4], "O");
+        assert_eq!(atoms[4], "e");
+        assert_eq!(atoms[5], "O");
+        assert_eq!(atoms[6], "e");
     }
 
     #[test]
@@ -107,12 +221,7 @@ mod tests {
 
     #[test]
     fn test_parse_all() {
-        let (replacements, molecule) = parse_all(
-            r#"H => HO
-            H => OH
-            O => HH
-
-            HOH"#);
+        let (replacements, molecule) = parse_all(SAMPLE);
 
         assert_eq!(replacements.len(), 3);
 
@@ -130,15 +239,10 @@ mod tests {
 
     #[test]
     fn test_build_molecules() {
-        let (replacements, sample_molecule) = parse_all(
-            r#"H => HO
-            H => OH
-            O => HH
-
-            HOH"#);
+        let (replacements, sample_molecule) = parse_all(SAMPLE);
 
 
-        let molecules = build_molecules(sample_molecule, replacements);
+        let molecules = build_molecules(sample_molecule, &replacements);
 
         assert_eq!(molecules.len(), 4);
         assert!(molecules.contains("HOOH"));
@@ -149,25 +253,16 @@ mod tests {
 
     #[test]
     fn test_build_molecules_santa() {
-        let (replacements, sample_molecule) = parse_all(
-            r#"H => HO
-            H => OH
-            O => HH
+        let (replacements, sample_molecule) = parse_all(SANTA_SAMPLE);
 
-            HOHOHO"#);
-
-        let molecules = build_molecules(sample_molecule, replacements);
+        let molecules = build_molecules(sample_molecule, &replacements);
         println!("{:?}", molecules);
         assert_eq!(molecules.len(), 7);
     }
 
     #[test]
     fn test_count_unique_molecules() {
-        let answer = count_unique_molecules(r#"H => HO
-            H => OH
-            O => HH
-
-            HOHOHO"#);
+        let answer = count_unique_molecules(SANTA_SAMPLE);
 
         assert_eq!(answer, 7);
     }
@@ -177,5 +272,36 @@ mod tests {
         let answer = count_unique_molecules("");
 
         assert_eq!(answer, 0);
+    }
+
+    #[test]
+    fn test_convert_replacements() {
+        let (replacements, _) = parse_all(E_SAMPLE);
+        let convert = convert_replacements(&replacements);
+
+        assert_eq!(convert.len(), 3);
+        assert_eq!(convert.get("e").unwrap().len(), 2);
+        assert_eq!(convert.get("H").unwrap().len(), 2);
+        assert_eq!(convert.get("O").unwrap().len(), 1);
+    }
+
+    #[test]
+    fn test_build_molecule_from_e() {
+        let (replacements, molecule) = parse_all(E_SAMPLE);
+        let number_steps = build_molecule_from_e(molecule, &replacements);
+        assert_eq!(number_steps, 3);
+    }
+
+    #[test]
+    fn test_build_santa_molecule_from_e() {
+        let (replacements, molecule) = parse_all(E_SANTA_SAMPLE);
+        let number_steps = build_molecule_from_e(molecule, &replacements);
+        assert_eq!(number_steps, 6);
+    }
+
+    #[test]
+    fn test_count_molecules_from_electron() {
+        let answer = count_molecules_from_electron(E_SANTA_SAMPLE);
+        assert_eq!(answer, 6);
     }
 }
