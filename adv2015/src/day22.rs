65b97806215ddd6527bf89fea8f439423f772be3
Vest <Vest@users.noreply.github.com>
2020-11-08 17:15:17 +0100

    Day 22 / 2015: added a big number class, it allows to create long number and increment them by 1
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- adv2015/src/day22.rs
+++ adv2015/src/day22.rs
@@ -1,3 +1,11 @@
+enum Action {
+    MagickMissile,
+    Drain,
+    Shield,
+    Poison,
+    Recharge,
+}
+
 struct Human {
     health: usize,
     damage: usize,
@@ -34,9 +42,70 @@ fn parse_enemy(input: &str) -> Human {
 }
 
 struct Generator {
-    counter: u32,
+    counter: Vec<u8>,
+    queue_length: u32,
 }
 
+impl Generator {
+    fn new() -> Generator {
+        Generator {
+            counter: vec![0, 10],
+            queue_length: 10,
+        }
+    }
+}
+
+impl Iterator for Generator {
+    type Item = Vec<Action>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
+
+struct BigNumber {
+    counter: Vec<u8>,
+    capped_value: u8,
+}
+
+impl BigNumber {
+    fn new(capped_value: u8, length: usize) -> BigNumber {
+        BigNumber {
+            counter: vec![1; length],
+            capped_value,
+        }
+    }
+}
+
+impl Iterator for BigNumber {
+    type Item = Vec<u8>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.counter.iter()
+            .filter(|digit| **digit == self.capped_value)
+            .count() == self.counter.len() {
+            return None;
+        }
+
+        let mut next = false;
+        let counter_length = self.counter.len();
+
+        self.counter[counter_length - 1] += 1;
+        for i in (0..counter_length).rev() {
+            if next {
+                self.counter[i] += 1;
+                next = false;
+            }
+
+            if self.counter[i] > self.capped_value {
+                next = true;
+                self.counter[i] -= self.capped_value;
+            }
+        }
+
+        Some(self.counter.clone())
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -52,5 +121,29 @@ mod tests {
         assert_eq!(enemy.health, 71);
         assert_eq!(enemy.damage, 10);
         assert_eq!(enemy.armor, 0);
+    }
+
+    #[test]
+    fn test_big_number_small() {
+        let mut big = BigNumber::new(2, 2);
+        assert_eq!(big.counter, vec![1, 1]);
+        assert_eq!(big.next(), Some(vec![1, 2]));
+        assert_eq!(big.next(), Some(vec![2, 1]));
+        assert_eq!(big.next(), Some(vec![2, 2]));
+        assert_eq!(big.next(), None);
+    }
+
+    #[test]
+    fn test_big_number_big() {
+        let mut big = BigNumber::new(3, 3);
+        assert_eq!(big.counter, vec![1, 1, 1]);
+        big.next();
+        big.next();
+        assert_eq!(big.next(), Some(vec![1, 2, 1]));
+        assert_eq!(big.next(), Some(vec![1, 2, 2]));
+        big.next();
+        big.next();
+        big.next();
+        assert_eq!(big.next(), Some(vec![1, 3, 3]));
     }
 }
