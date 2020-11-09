5a503ed7dbbc26abc56c97378ac8c5d9f7e228f4
Vest <Vest@users.noreply.github.com>
2020-11-09 12:58:28 +0100

    Day 22 / 2015: added queue of actions
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- adv2015/src/day22.rs
+++ adv2015/src/day22.rs
@@ -1,15 +1,56 @@
 enum Action {
-    MagickMissile,
-    Drain,
-    Shield,
-    Poison,
-    Recharge,
+    // 1
+    MagickMissile {
+        cost: usize,
+        damage: usize,
+    },
+
+    // 2
+    Drain {
+        cost: usize,
+        damage: usize,
+        heal: usize,
+    },
+
+    // 3
+    Shield {
+        cost: usize,
+        duration: usize,
+        armor: usize,
+    },
+
+    // 4
+    Poison {
+        cost: usize,
+        duration: usize,
+        damage: usize,
+    },
+
+    // 5
+    Recharge {
+        cost: usize,
+        duration: usize,
+        mana: usize,
+    },
 }
 
 struct Human {
     health: usize,
     damage: usize,
     armor: usize,
+}
+
+impl Action {
+    fn from_u8(n: u8) -> Option<Self> {
+        match n {
+            1 => Some(Action::MagickMissile { cost: 53, damage: 4 }),
+            2 => Some(Action::Drain { cost: 73, damage: 2, heal: 2 }),
+            3 => Some(Action::Shield { cost: 113, duration: 6, armor: 7 }),
+            4 => Some(Action::Poison { cost: 173, duration: 6, damage: 3 }),
+            5 => Some(Action::Recharge { cost: 229, duration: 5, mana: 101 }),
+            _ => None,
+        }
+    }
 }
 
 impl Default for Human {
@@ -42,15 +83,13 @@ fn parse_enemy(input: &str) -> Human {
 }
 
 struct Generator {
-    counter: Vec<u8>,
-    queue_length: u32,
+    counter: BigNumber,
 }
 
 impl Generator {
-    fn new() -> Generator {
+    fn new(queue_length: usize) -> Generator {
         Generator {
-            counter: vec![0, 10],
-            queue_length: 10,
+            counter: BigNumber::new(5, queue_length),
         }
     }
 }
@@ -107,6 +146,45 @@ impl Iterator for BigNumber {
     }
 }
 
+fn is_queue_valid(queue: Vec<Action>) -> bool {
+    let mut shield_state = (0usize, false);
+    let mut poison_state = (0usize, false);
+    let mut recharge_state = (0usize, false);
+
+    for action in queue {
+        shield_state.0 = shield_state.0.saturating_sub(1);
+        poison_state.0 = poison_state.0.saturating_sub(1);
+        recharge_state.0 = recharge_state.0.saturating_sub(1);
+
+        match action {
+            Action::Shield { duration, .. } => {
+                if shield_state.1 {
+                    return false;
+                }
+                shield_state.0 = duration;
+                shield_state.1 = true;
+            }
+            Action::Poison { duration, .. } => {
+                if poison_state.1 {
+                    return false;
+                }
+                poison_state.0 = duration;
+                poison_state.1 = true;
+            }
+            Action::Recharge { duration, .. } => {
+                if recharge_state.1 {
+                    return false;
+                }
+                recharge_state.0 = duration;
+                recharge_state.1 = true;
+            }
+            _ => {}
+        }
+    }
+
+    true
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -145,5 +223,28 @@ mod tests {
         big.next();
         big.next();
         assert_eq!(big.next(), Some(vec![1, 3, 3]));
+    }
+
+    #[test]
+    fn test_from_u8() {
+        assert!(Action::from_u8(0).is_none());
+        assert!(Action::from_u8(1).is_some());
+        assert!(Action::from_u8(2).is_some());
+        assert!(Action::from_u8(3).is_some());
+        assert!(Action::from_u8(4).is_some());
+        assert!(Action::from_u8(5).is_some());
+        assert!(Action::from_u8(6).is_none());
+    }
+
+    #[test]
+    fn test_is_queue_valid() {
+        assert!(is_queue_valid(vec![Action::from_u8(1).unwrap(), Action::from_u8(2).unwrap()]));
+        assert!(is_queue_valid(vec![Action::from_u8(2).unwrap(), Action::from_u8(3).unwrap()]));
+        assert!(!is_queue_valid(vec![Action::from_u8(3).unwrap(), Action::from_u8(3).unwrap()]));
+        assert!(is_queue_valid(vec![Action::from_u8(3).unwrap(), Action::from_u8(4).unwrap()]));
+        assert!(is_queue_valid(vec![Action::from_u8(4).unwrap(), Action::from_u8(3).unwrap()]));
+        assert!(!is_queue_valid(vec![Action::from_u8(4).unwrap(), Action::from_u8(4).unwrap()]));
+        assert!(!is_queue_valid(vec![Action::from_u8(5).unwrap(), Action::from_u8(5).unwrap()]));
+        assert!(is_queue_valid(vec![Action::from_u8(3).unwrap(), Action::from_u8(4).unwrap(), Action::from_u8(5).unwrap()]));
     }
 }
