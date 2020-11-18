e60e02564d3ba86c0c8700a9b21a507aa518259e
Vest <Vest@users.noreply.github.com>
2020-11-18 17:57:40 +0100

    Day 24 / 2015: create groups

--- adv2015/src/day24.rs
+++ adv2015/src/day24.rs
@@ -18,6 +18,25 @@ fn create_groups(packages: &Vec<usize>, size: usize) -> Vec<Vec<usize>> {
     Combinations::new(copy_packages, size).collect()
 }
 
+fn find_groups_with_weight(packages: &Vec<usize>, weight: usize) -> Vec<Vec<usize>> {
+    let mut result = Vec::new();
+
+    if packages.is_empty() {
+        return result;
+    }
+
+    for size in 1..packages.len() {
+        create_groups(&packages, size).iter()
+            .filter(|group| group.iter().sum::<usize>() == weight)
+            .for_each(|group| {
+                let copy_group = group.to_vec();
+                result.push(copy_group);
+            });
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -42,5 +61,14 @@ mod tests {
         assert_eq!(group_1.len(), 4 * 3 * 2 / (2 * 2));
         assert!(create_groups(&available_packages, 0).is_empty());
         assert!(create_groups(&available_packages, 5).is_empty());
+    }
+
+    #[test]
+    fn test_find_groups_with_weight() {
+        let packages: Vec<usize> = vec![1, 2, 3, 4, 5, 7, 8, 9, 10, 11];
+        let found_packages = find_groups_with_weight(&packages, packages.iter().sum::<usize>() / 3);
+
+        assert_eq!(found_packages.len(), 25); // taken from demo (indeed in the demo there are 13 combinations)
+        println!("{:?}", found_packages);
     }
 }
