98b63e0679e45eb5e99a6f28e9b82bc6b70531b1
Vest <Vest@users.noreply.github.com>
2020-11-30 12:27:43 +0100

    Advent: prepared the project for 2020. Added better arguments handling with "clap"
    Signed-off-by: Vest <Vest@users.noreply.github.com>

---
+++ adv2020/src/day1.rs
@@ -0,0 +1,19 @@
+pub fn answer1(input: &str) -> i16 {
+    0
+}
+
+pub fn answer2(input: &str) -> i16 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_answers() {
+        assert_eq!(answer1(""), 0);
+        assert_eq!(answer2(""), 0);
+    }
+}
+
