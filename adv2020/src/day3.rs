c9d0f1071f68f4cdc9cc50b4b69c6a774694c1e1
Vest <Vest@users.noreply.github.com>
2020-12-03 06:23:58 +0100

    Day 3 / 2020: 187 and 4723283400 This is a dirty code.

--- adv2020/src/day3.rs
+++ adv2020/src/day3.rs
@@ -1,13 +1,69 @@
-pub fn answer1(input: &str) -> i16 {
-    0
+pub fn answer1(input: &str) -> usize {
+    let input = parse_input(input);
+    let mut x_pos = 0usize;
+    let mut count = 0usize;
+    for line in input {
+        if line.chars().nth(x_pos % line.len()).unwrap() == '#' {
+            count += 1;
+        }
+        x_pos += 3;
+    }
+
+    count
 }
 
-pub fn answer2(input: &str) -> i16 {
-    0
+pub fn answer2(input: &str) -> usize {
+    let input = parse_input(input);
+
+    let mut x_pos_1 = 0usize;
+    let mut x_pos_3 = 0usize;
+    let mut x_pos_5 = 0usize;
+    let mut x_pos_7 = 0usize;
+
+    let mut x_pos_2 = 0usize;
+    let mut y_pos_2 = 2usize;
+
+    let mut count_1 = 0usize;
+    let mut count_3 = 0usize;
+    let mut count_5 = 0usize;
+    let mut count_7 = 0usize;
+    let mut count_2_down = 0usize;
+
+    for line in input.iter().enumerate() {
+        if line.1.chars().nth(x_pos_1 % line.1.len()).unwrap() == '#' {
+            count_1 += 1;
+        }
+        if line.1.chars().nth(x_pos_3 % line.1.len()).unwrap() == '#' {
+            count_3 += 1;
+        }
+        if line.1.chars().nth(x_pos_5 % line.1.len()).unwrap() == '#' {
+            count_5 += 1;
+        }
+        if line.1.chars().nth(x_pos_7 % line.1.len()).unwrap() == '#' {
+            count_7 += 1;
+        }
+
+        if line.0 == y_pos_2 {
+            x_pos_2 += 1;
+            y_pos_2 += 2;
+            if line.1.chars().nth(x_pos_2 % line.1.len()).unwrap() == '#' {
+                count_2_down += 1;
+            }
+        }
+
+        x_pos_1 += 1;
+        x_pos_3 += 3;
+        x_pos_5 += 5;
+        x_pos_7 += 7;
+    }
+
+    count_1 * count_3 * count_5 * count_7 * count_2_down
 }
 
-fn parse_input(input: &str) -> Vec<i16> {
-    vec![0]
+fn parse_input(input: &str) -> Vec<String> {
+    input.lines()
+        .map(|l| String::from(l.trim()))
+        .collect()
 }
 
 #[cfg(test)]
