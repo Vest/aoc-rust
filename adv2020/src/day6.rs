d779ecc7bb2eec5f411bee25170e24d3da724562
Vest <Vest@users.noreply.github.com>
2020-12-06 06:23:51 +0100

    Day 6 / 2020: 6335 and 3392 Dirty but quick solution (as always during these challenges)
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- adv2020/src/day6.rs
+++ adv2020/src/day6.rs
@@ -1,17 +1,59 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn find_answer1(input: &str) -> usize {
-    0
+    let mut result = 0usize;
+
+    let mut set: HashSet<char> = HashSet::new();
+
+    for line in input.lines() {
+        if line.is_empty() {
+            result += set.len();
+            set.clear();
+        } else {
+            line.chars()
+                .for_each(|c: char| {
+                    set.insert(c);
+                })
+        }
+    }
+
+    result + set.len()
 }
 
 pub fn find_answer2(input: &str) -> usize {
-    0
+    let mut result = 0usize;
+
+    let mut map_stats: HashMap<char, usize> = HashMap::new();
+    let mut lines = 0usize;
+
+    for line in input.lines() {
+        if line.is_empty() {
+            result += map_stats.iter()
+                .filter(|&(_, v)| *v == lines)
+                .count();
+            lines = 0;
+            map_stats.clear();
+        } else {
+            line.chars()
+                .for_each(|c: char| {
+                    if map_stats.contains_key(&c) {
+                        let mut value = map_stats.get(&c).unwrap();
+                        let value = *value + 1;
+                        map_stats.insert(c, value);
+                    } else {
+                        map_stats.insert(c, 1);
+                    }
+                });
+
+            lines += 1;
+        }
+    }
+
+    result + map_stats.iter()
+        .filter(|&(_, v)| *v == lines)
+        .count()
 }
-/*
-fn parse_input<'a>(input: &'a str) -> impl Iterator<Item=Seat> + 'a {
-    input.lines()
-        .map(&str::trim)
-        .map(parse_seat)
-}
-*/
+
 
 #[cfg(test)]
 mod tests {
@@ -20,7 +62,21 @@ mod tests {
 
     #[test]
     fn test_empty_answers() {
-        assert_eq!(find_answer1(""), 0);
+        assert_eq!(find_answer1(r#"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"#), 11);
         assert_eq!(find_answer2(""), 0);
     }
 }
