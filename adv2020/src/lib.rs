592ad198aade3e141f35a9dbd64cfb15eacfe667
Vest <Vest@users.noreply.github.com>
2020-12-13 09:39:15 +0100

    Day 10 / 2020: tried to fix tests
    Signed-off-by: Vest <Vest@users.noreply.github.com>

--- adv2020/src/lib.rs
+++ adv2020/src/lib.rs
@@ -11,6 +11,7 @@ mod day9;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 /* </editor-fold> */
 
 pub fn print_answers(day: u8, input: String) {
@@ -28,8 +29,9 @@ pub fn print_answers(day: u8, input: String) {
         8 => println!("{} {} and {}", prefix, day8::execute_first_program(input), day8::execute_second_program(input)),
         9 => println!("{} {} and {}", prefix, day9::find_weak_number_25(input), day9::find_sum_of_any_numbers(input)),
         10 => println!("{} {} and {}", prefix, day10::find_one_by_three(input), day10::find_answer2(input)),
-        11 => println!("{} {} and {}", prefix, day11::find_answer1(input), day11::find_answer2(input)),
+        // 11 => println!("{} {} and {}", prefix, day11::find_answer1(input), day11::find_answer2(input)),
         12 => println!("{} {} and {}", prefix, day12::find_answer1(input), day12::find_answer2(input)),
+        13 => println!("{} {} and {}", prefix, day13::find_answer1(input), day13::find_answer2(input)),
        // 9 => println!("{} {} and {}", prefix, day9::find_answer1(input), day9::find_answer2(input)),
 
         _ => { eprintln!("2020: I don't know the answer for day {} :(", day) }
